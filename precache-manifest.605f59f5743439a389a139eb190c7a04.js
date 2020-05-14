02fba61e6695599083c24bf622761fdd87870cc5
Yesme <moralesyesme@gmail.com>
2020-05-14 16:33:46 -0500

    Updates

---
+++ precache-manifest.605f59f5743439a389a139eb190c7a04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e758487cfa7a7f4e735c4c6bb57bf2",
+    "url": "/reactjs-social-media-dashboard/index.html"
+  },
+  {
+    "revision": "be3c7f99642c0a4e8060",
+    "url": "/reactjs-social-media-dashboard/static/css/main.b66a54c6.chunk.css"
+  },
+  {
+    "revision": "f9781a2ba1b815aa9e05",
+    "url": "/reactjs-social-media-dashboard/static/js/2.1323efed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-social-media-dashboard/static/js/2.1323efed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3c7f99642c0a4e8060",
+    "url": "/reactjs-social-media-dashboard/static/js/main.e761ccc8.chunk.js"
+  },
+  {
+    "revision": "4edbfeb578b8c17ccf0a",
+    "url": "/reactjs-social-media-dashboard/static/js/runtime-main.70d3422b.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/reactjs-social-media-dashboard/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/reactjs-social-media-dashboard/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/reactjs-social-media-dashboard/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/reactjs-social-media-dashboard/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/reactjs-social-media-dashboard/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/reactjs-social-media-dashboard/static/media/icon-youtube.8499da12.svg"
+  }
+]);
